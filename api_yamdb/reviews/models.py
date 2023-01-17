948643a95ac331a7ffbb3301a636301ded728ba5
Рома Овинников <romanovinnikov@yandex.ru>
2023-01-17 19:30:49 +0300

    fixes

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -9,37 +9,65 @@ class Reviews(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='reviews'
+        related_name='reviews',
+        verbose_name='Автор отзыва',
     )
-    title_id = models.IntegerField()
+    title_id = models.IntegerField(
+        verbose_name='ID произведения',
+    )
     # Заменить IntegerField на ForeignKey после создания модели произведений
-    text = models.TextField()
+    text = models.TextField(
+        verbose_name='Текст отзыва',
+    )
     score = models.IntegerField(
         validators=[
             MinValueValidator(0),
             MaxValueValidator(10)
-        ]
+        ],
+        verbose_name='Оценка',
     )
-    pub_date = pub_date = models.DateTimeField(
+    pub_date = models.DateTimeField(
         auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата публикации',
     )
 
+    def __str__(self) -> str:
+        return f'{self.text[:15]}'
+
     class Meta:
-        unique_together = ('author', 'title_id')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title_id'],
+                name='unique_reviews'
+            )
+        ]
 
 
 class Comments(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='comments'
+        related_name='comments',
+        verbose_name='Автор коммента',
     )
     review_id = models.ForeignKey(
         Reviews,
         on_delete=models.CASCADE,
-        related_name='review'
+        related_name='review',
+        verbose_name='ID отзыва',
     )
-    text = models.TextField()
-    pub_date = pub_date = models.DateTimeField(
+    text = models.TextField(
+        verbose_name='Текст комментария',
+    )
+    pub_date = models.DateTimeField(
         auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата публикации комментария',
     )
+
+    def __str__(self) -> str:
+        return f'{self.text[:15]}'
+    
+    class Meta():
+        ordering = ['-pub_date']
