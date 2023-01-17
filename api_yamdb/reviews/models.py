16cba88453d45ba71d56b40ec5275a40453f9bb0
Рома Овинников <romanovinnikov@yandex.ru>
2023-01-17 17:21:56 +0300

    added Comment and Reviews models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Reviews(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    title_id = models.IntegerField()
+    # Заменить IntegerField на ForeignKey после создания модели произведений
+    text = models.TextField()
+    score = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(10)
+        ]
+    )
+    pub_date = pub_date = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        unique_together = ('author', 'title_id')
+
+
+class Comments(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    review_id = models.ForeignKey(
+        Reviews,
+        on_delete=models.CASCADE,
+        related_name='review'
+    )
+    text = models.TextField()
+    pub_date = pub_date = models.DateTimeField(
+        auto_now_add=True,
+    )
